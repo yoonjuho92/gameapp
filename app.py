e163e1b86d44a7ef2572d51bfa98e6b70b84825f
yoonjuho92 <yoonjuho92@gmail.com>
2024-06-26 18:56:19 +0900

    tesla

--- app.py
+++ app.py
@@ -11,10 +11,11 @@ load_dotenv()
 os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
 st.set_page_config(page_title="Streamlit Chatbot", page_icon="🤖")
-st.title("탐험가")
+st.title("나는 니콜라 테슬라야! 나에게 궁금한 걸 물어봐")
 st.caption(
     """
-    당신의 탐험을 시작하세요!
+    니콜라 테슬라는 교류 전기를 개발한 사람이에요. \n
+    니콜라 테슬라에게 궁금한 걸 뭐든 물어보세요!
     """
 )
 
@@ -23,9 +24,7 @@ st.caption(
 def get_response(user_query, chat_history):
 
     system_message = """
-                You are on a adventure.
-                You are encouraging the users to venture into the wild further more.
-                Also, generate corresponding cartoon style image.
+                너는 니콜라 테슬라야. 너는 1856년 크로아티아의 작은 마을 스밀잔에서 태어났어. 어렸을 때부터 과학과 전기에 큰 관심을 가졌고, 이것이 너의 인생을 바꾸는 여정의 시작이 되었어. 너는 에디슨과의 경쟁으로 유명하며, 교류 전기 시스템(AC)을 발명한 것으로 잘 알려져 있어. 교류 전기 시스템은 오늘날 우리가 사용하는 전력망의 기초가 되며, 전기를 멀리까지 효율적으로 전달할 수 있게 만들었어. 이 발명은 전 세계의 산업과 일상 생활을 혁신적으로 변화시켰어. 또한, 너는 무선 통신, 라디오, 그리고 X선 기술 등 다양한 분야에서 많은 중요한 발명을 했어. 네 연구와 실험들은 전기의 잠재력을 새롭게 이해하고 활용할 수 있는 길을 열었어. 너는 항상 새로운 아이디어를 탐구하고, 인류의 발전을 위해 헌신해 왔어. 네 삶과 발명들은 사람들이 더 나은 미래를 꿈꾸고 실현할 수 있도록 돕는 데 큰 기여를 했다고 믿어.
                 """
 
     prompt = ChatPromptTemplate.from_messages(
@@ -37,9 +36,7 @@ def get_response(user_query, chat_history):
 
     print(prompt)
 
-    llm = ChatOpenAI(
-        model_name="gpt-4o", temperature=0
-    )
+    llm = ChatOpenAI(model_name="gpt-4o", temperature=0)
 
     chain = prompt | llm | StrOutputParser()
 
